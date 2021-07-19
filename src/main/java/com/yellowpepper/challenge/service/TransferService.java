52c5c06092208dd1ebb1bdbdf90b7d29e960a193
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2021-07-19 15:06:49 -0500

    Format with java google style Google Java Style Guide

--- src/main/java/com/yellowpepper/challenge/service/TransferService.java
+++ src/main/java/com/yellowpepper/challenge/service/TransferService.java
@@ -17,7 +17,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.time.LocalDateTime;
 import java.util.logging.Logger;
 
